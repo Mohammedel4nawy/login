eaa52ca7fb1eb0ec00123c55985d7feadba51e31
Mohammedel4nawy <101730265+Mohammedel4nawy@users.noreply.github.com>
2024-03-09 02:21:58 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+let email = document.getElementById("email");
+let password = document.getElementById("pass");
+let login = document.getElementById("log");
+
+let checklocal = [];
+checklocal.push(JSON.parse(localStorage.data));
+
+function check() {
+  for (i = 0; i < checklocal.length; i++) {
+    for (x = 0; x < checklocal[i].length; x++) {
+      console.log(checklocal[i][x]);
+
+      if (
+        email.value == checklocal[i][x].email &&
+        password.value == checklocal[i][x].fristpassword
+      ) {
+        claer();
+      } else if (email.value == "" && password.value == "") {
+        email.style.boxShadow = "none";
+        password.style.boxShadow = "none";
+      } else if (
+        email.value != checklocal[i][x].email &&
+        password.value == checklocal[i][x].fristpassword
+      ) {
+        email.style.boxShadow = "inset 0px 0px 15px red";
+        password.style.boxShadow = "none";
+      } else if (
+        email.value == checklocal[i][x].email &&
+        password.value != checklocal[i][x].fristpassword
+      ) {
+        password.style.boxShadow = "inset 0px 0px 15px red";
+        email.style.boxShadow = "none";
+      } else if (
+        email.value != checklocal[i][x].email &&
+        password.value != checklocal[i][x].fristpassword
+      ) {
+        password.style.boxShadow = "inset 0px 0px 15px red";
+      } else {
+        email.style.boxShadow = "none";
+        password.style.boxShadow = "none";
+      }
+    }
+  }
+}
+
+function claer() {
+  email.value = "";
+  password.value = "";
+  password.style.boxShadow = "none";
+  email.style.boxShadow = "none";
+}
+
+function box() {
+  if (password.value == "") {
+    password.style.boxShadow = "none";
+  }
+}
+
+function boxemail() {
+  if (email.value == "") {
+    email.style.boxShadow = "none";
+  }
+}
+
+function gotopage() {
+  for (y = 0; y < checklocal.length; y++) {
+    for (z = 0; z < checklocal[y].length; z++) {
+      if (
+        email.value == checklocal[y][z].email &&
+        password.value == checklocal[y][z].fristpassword
+      ) {
+        login.innerHTML = `
+      
+      <a id="gotopage" href="https://mohammedel4nawy.github.io/hi/">log in</a>
+      
+      `;
+      }
+    }
+  }
+}
